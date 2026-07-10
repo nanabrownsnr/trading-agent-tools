eac812374336e5044bc2e213901f6063c3f2bc12
Nana Brown <nanabrownsnr@gmail.com>
2026-07-10 16:14:24 +0000

    syntax update

--- postgres_mcp/server.py
+++ postgres_mcp/server.py
@@ -8,9 +8,7 @@ import re
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=[
-        logging.StreamHandler(),  # Prints to your terminal console
-        ],
+    handlers=[logging.StreamHandler()]
 )
 
 load_dotenv()
