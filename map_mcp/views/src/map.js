4250edeb3c2c95da62662836c6bf397223a31d21
Nana Brown <nanabrownsnr@gmail.com>
2026-07-13 17:16:18 +0000

    fixed pointer by importing imgaes

--- map_mcp/views/src/map.js
+++ map_mcp/views/src/map.js
@@ -1,6 +1,16 @@
 import { App } from "@modelcontextprotocol/ext-apps";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+});
 
 
 const app = new App({ name: "map_mcp", version: "1.0.0" });
